7f04e094fb95692f59352e075948a2a2e185dd79
Thiago Vinhote <thiagovf26@outlook.com>
2017-12-07 18:24:07 -0400

    atualizaçõo dos recursos
    Signed-off-by: Thiago Vinhote <thiagovf26@outlook.com>

--- models/dao/IdeiaDAO.php
+++ models/dao/IdeiaDAO.php
@@ -30,12 +30,12 @@ class IdeiaDAO implements DAOInterface {
             return $result->fetch_assoc();
         }
     }
-    
+
     public function save($object){
-        $sql = "INSERT INTO ideia (nome, descricao, data_criacao, usuario_id) 
+        $sql = "INSERT INTO ideia (nome, descricao, data_criacao, usuario_id)
         VALUES ('".$object->getNome()."', '".$object->getDescricao()."', '".$object->getDataCriacao()."', '".$object->getUserId()."' )
         ";
-    
+
         $resultado = $this->conexao->query($sql);
         if(!$resultado){
             return null;
@@ -47,7 +47,7 @@ class IdeiaDAO implements DAOInterface {
 
     public function update($object, $id){
         $sql = "UPDATE ideia SET nome = '".$object->getNome()."', descricao = '".$object->getDescricao()."', data_criacao = '".$object->getDataCriacao()."', usuario_id = '".$object->getUserId()."' WHERE id = $id";
-        
+
         $result = $this->conexao->query($sql);
         if(!$result){
             return null;
@@ -66,36 +66,35 @@ class IdeiaDAO implements DAOInterface {
         $data = new stdClass();
         $data->nome = 'Ideia';
         $data->endpoint = 'http://localhost:8080/padawan-ideas-api/ideia';
-    
+
         $recursos = new stdClass();
         $recursos = [
           array('action' => 'POST',
             'fields' =>
               [
-                array('field' => 'nome', 'type' => 'string','required' => 'true'),
-                array('field' => 'descricao', 'type' => 'string','required' => 'true'),
-                array('field' => 'data_criacao', 'type' => 'date', 'required' => 'false'),
-                array('field' => 'usuario_id', 'type' => 'int', 'required' => 'true'),
-              ] 
+                array('field' => 'nome', 'type' => 'string','required' => true, 'max_length' => 100),
+                array('field' => 'descricao', 'type' => 'string','required' => true, 'max_length' => 200),
+                array('field' => 'data_criacao', 'type' => 'date', 'required' => false),
+                array('field' => 'usuario_id', 'type' => 'int', 'required' => true),
+              ]
               ),
           array('action' => 'GET',
             'fields' =>
               [
-                array('field' => 'id', 'type' => 'int','required' => 'false')
-              ] 
+                array('field' => 'id', 'type' => 'int', 'required' => false)
+              ]
               ),
           array('action' => "PUT",
             'fields' =>
             [
-              array('field' => 'id', 'type' => 'int', 'required' => 'true'),
-              array('field' => 'nome', 'type' => 'string', 'required' => 'false'),
-              array('field' => 'descricao', 'type' => 'string', 'required' => 'false'),
-              array('field' => 'data_criacao', 'type' => 'date', 'required' => 'false'),
-              array('field' => 'usuario_id', 'type' => 'int', 'required' => 'false')
+              array('field' => 'nome', 'type' => 'string', 'required' => false, 'max_length' => 100),
+              array('field' => 'descricao', 'type' => 'string', 'required' => false, 'max_length' => 200),
+              array('field' => 'data_criacao', 'type' => 'date', 'required' => false),
+              array('field' => 'usuario_id', 'type' => 'int', 'required' => false)
             ]
           )
           ];
-        
+
         $data->recursos = $recursos;
         return $data;
     }
